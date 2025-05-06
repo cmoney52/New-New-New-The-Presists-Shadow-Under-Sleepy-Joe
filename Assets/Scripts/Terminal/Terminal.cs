868b429bb57bc2f7cb4fcb36784e0069ae60b926
dannyhenningfeld <159461940+dannyhenningfeld@users.noreply.github.com>
2025-05-06 08:48:06 -0500

    Sleepy Time Updates

---
+++ Assets/Scripts/Terminal/Terminal.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Terminal : MonoBehaviour
+{
+    public GameObject WholeUI;
+    public Button close;
+    public GameObject playerCamera;
+    public GameObject playerMovement;
+    
+
+    void OnEnable()
+    {
+        if (WholeUI.activeSelf)
+        {
+            FirstPersonLook Look = playerCamera.GetComponent<FirstPersonLook>();
+            FirstPersonMovement Move = playerMovement.GetComponent<FirstPersonMovement>();
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            Look.enabled = false;
+            Move.enabled = false;
+
+        }
+    }
+
+    void OnDisable()
+    {
+        FirstPersonLook Look = playerCamera.GetComponent<FirstPersonLook>();
+        FirstPersonMovement Move = playerMovement.GetComponent<FirstPersonMovement>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Look.enabled = true;
+        Move.enabled = true;
+    }
+
+    private void Start()
+    {
+        close.onClick.AddListener(closeMenu);
+    }
+
+    public void closeMenu()
+    {
+
+        WholeUI.gameObject.SetActive(false);
+    }
+}
