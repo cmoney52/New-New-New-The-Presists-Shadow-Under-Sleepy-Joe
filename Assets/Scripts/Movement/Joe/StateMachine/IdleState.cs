35226bf1320d1ffbf7d4c8605dc8a5937c741078
cmoney52 <149429981+cmoney52@users.noreply.github.com>
2025-05-19 10:58:54 -0500

    Some More Updates

--- Assets/Scripts/Movement/Joe/StateMachine/IdleState.cs
+++ Assets/Scripts/Movement/Joe/StateMachine/IdleState.cs
@@ -1,10 +1,17 @@
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class IdleState : State
 {
     public ChaseState chaseState;
+    public NavMeshAgent agent;
+    public Transform WaypointFolder;
+    private Transform[] waypoints;
+    private int currentWaypoint = 0;
+
+
     void Start()
     {
         if (fpvController != null)
@@ -12,6 +19,34 @@ public class IdleState : State
             playerMovementScript = fpvController.GetComponent<FirstPersonMovement>(); // Get First Person Movement script
         }
 
+    }
+
+    void OnEnable()
+    {
+        while (GetComponentInParent<NavMeshAgent>() == null) { }
+        agent = GetComponentInParent<NavMeshAgent>();
+        waypoints = new Transform[WaypointFolder.childCount];
+        for (int i = 0; i < WaypointFolder.childCount; i++)
+        {
+            waypoints[i] = WaypointFolder.GetChild(i);
+        }
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 1.5F)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+            agent.SetDestination(waypoints[currentWaypoint].position);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (agent != null && agent.hasPath)
+        {
+            agent.ResetPath();
+        }
     }
 
     public override State RunCurrentState()
@@ -28,3 +63,5 @@ public class IdleState : State
 
     }
 }
+
+
