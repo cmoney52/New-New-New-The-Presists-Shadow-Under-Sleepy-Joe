79ef37c7845a5b55ae8907594f8e028ab8733c99
BlakeB07 <bbradbur69@students.kusd.edu>
2025-05-28 08:27:48 -0500

    Bababooey

--- Assets/Scripts/Movement/Joe/StateMachine/AttackState.cs
+++ Assets/Scripts/Movement/Joe/StateMachine/AttackState.cs
@@ -10,6 +10,9 @@ public class AttackState : State
     public Vector3 teleportPositionLocation; // Ensure this is assigned properly
     private FirstPersonMovement attackPlayerMovementScript;
     private bool isReleasingPlayer = false;
+    public Transform dungeonButtons;
+    public Transform[] buttonsFolder;
+    private Interactable interactScript;
 
     private void Start()
     {
@@ -40,16 +43,32 @@ public class AttackState : State
         rb.constraints = RigidbodyConstraints.FreezeRotation;
         myPlayer.localPosition = new Vector3(0f, 1f, 0.5f);
 
+        
+
+        for (int i = 0; i < dungeonButtons.childCount; i++)
+        {
+            buttonsFolder[i] = dungeonButtons.GetChild(i);
+        }
+
+        int randomInt = Random.Range(1, dungeonButtons.childCount);
+
+        Interactable buttonScript = buttonsFolder[randomInt].GetComponent<Interactable>();
+        buttonScript.enabled = true;
+
         if (holdingPlayer)
         {
-            //
-
             // Start releasing the player using PlayerHoldScripts
             PlayerHoldScript.Instance.StartReleasePlayerCoroutine(
                 myPlayer, playerParentObject, waitTime, attackPlayerMovementScript, rb, teleportPosition,
                 () => { isReleasingPlayer = false; } // Reset flag after coroutine finishes
             );
 
+            //re-disable all dungeon buttons
+            for (int i = 0; i < dungeonButtons.childCount; i++)
+            {
+                Interactable InteractScript = buttonsFolder[i].GetComponent<Interactable>();
+                InteractScript.enabled = false;
+            }
             return idleState;
         }
 
